ca484be88bc98bf16b4e0ad1beb651e353ec37c3
Ravneet <ravneet.virk@gmail.com>
2023-01-11 21:15:42 -0500

    Save hello.js

---
+++ hello.js
@@ -0,0 +1,14 @@
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+//
+const sayHello1  = function (name) {
+  console.log("Hello, " + name);
+
+sayHello1("Caliban");
+sayHello1("Miranda");
+sayHello1("Ferdinand");
+
+}
